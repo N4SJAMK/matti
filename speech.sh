ca4784556d6b14e542dc2d79e360eddf09ef73bb
matti <melonmanchan@gmail.com>
2015-05-28 14:40:14 +0300

    Add language support to text to speech .sh

--- speech.sh
+++ speech.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=sv&q=$*"; }
-say "$*"
+MSG=$1
+LANG=$2
+/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=$LANG&q=$MSG"
+
